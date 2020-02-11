d522b44516109473861b071c2c81d9548a8218d3
Lordpas3 <Lordpas3@gmail.com>
2020-02-11 12:15:03 +0100

    website update db

---
+++ database/migrations/2020_02_11_102016_create_grades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('subject');
+            $table->text('test');
+            $table->float('grade')->nullable();
+            $table->float('weight')->nullable();
+            $table->float('ec')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
